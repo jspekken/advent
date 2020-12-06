fa7d785616cedda410dfa220f25cdd6ede2b7895
Jelle Spekken <jspekken@gmail.com>
2020-12-06 16:30:23 +0100

    starts part-2 of day 5

---
+++ 2020/5/src/part-2/index.js
@@ -0,0 +1,56 @@
+/**
+ * @see https://adventofcode.com/2020/day/5
+ */
+
+const utils = require('../../../shared/utils');
+const path = require('path');
+
+Array.prototype.splitInHalf = function () {
+  const lengthAtHalf = Math.ceil(this.length / 2);
+
+  const lower = this.slice(0, lengthAtHalf);
+  const upper = this.slice(lengthAtHalf);
+
+  return {
+    lower: lower.length ? lower : this,
+    upper: upper.length ? upper : this,
+  };
+}
+
+const range = (r) => [...Array(r).keys()];
+
+function getSeat(boardingPass) {
+  let rows = range(127);
+  let cols = range(7);
+
+  for (const character of boardingPass) {
+    if (character === 'F') {
+      rows = rows.splitInHalf().lower;
+    } else if (character === 'B') {
+      rows = rows.splitInHalf().upper;
+    } else if (character === 'L') {
+      cols = cols.splitInHalf().lower;
+    } else if (character === 'R') {
+      cols = cols.splitInHalf().upper;
+    }
+  }
+
+  return rows[0] * 8 + cols[0];
+}
+
+const input = utils.readInputSync(
+  path.resolve(__dirname, '../../resources/input.txt'),
+);
+
+const seats = input.map(getSeat);
+const min = Math.min(...seats);
+const max = Math.max(...seats);
+
+const missing = [];
+for (let i = min; i <= max; i++) {
+  if (!seats.includes(i)) {
+    missing.push(i);
+  }
+}
+
+console.log(missing);
